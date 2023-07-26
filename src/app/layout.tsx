4132da5b81361506057b553a617381e81a4cb124
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-07-26 10:31:59 -0300

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,32 @@ import React from 'react'
 import { fontSans } from '@/lib/fonts'
 import { Metadata } from 'next'
 import { Toaster } from '@/components/ui/toaster'
+import { env } from '@/env.mjs'
 
 export const metadata: Metadata = {
+  metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
   title: 'Conselho da Juventude 2023',
   description: 'Realize sua isncrição para o Conselho da Juventude 2023',
+  keywords: [
+    'Juventude',
+    'Cadesgo',
+    'Assembleia',
+    'Evento',
+    'Inscrição',
+    'Conselho da Juventude',
+    'Missão',
+    'Igreja',
+    'Jovens',
+    'Deus',
+    'Cristão',
+  ],
+  authors: [
+    {
+      name: 'samuelfr7',
+      url: 'https://github.com/samuelfr7',
+    },
+  ],
+  creator: 'samuelfr7',
   icons: {
     icon: '/favicon.ico',
   },
