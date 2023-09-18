9008c86e06c5da514c1e21d50ff0dc5e353bcaa6
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-09-18 00:38:29 -0300

    test: timezone sao paulo keep true

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -83,6 +83,6 @@ export function dateDifferenceFromToday(date: Date): number {
 export function subscriptionDeadlinePassed(): boolean {
   dayjs.extend(utc)
   dayjs.extend(timezone)
-  const deadline = dayjs('2023-09-18').tz('America/Sao_Paulo')
+  const deadline = dayjs('2023-09-18').tz('America/Sao_Paulo', true)
   return dayjs().isAfter(deadline, 'hour')
 }
